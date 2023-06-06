15349ee6b758dc7db6b6ce4c00b3764b9f7584ba
imorlxs <isaacmoralessantana@gmail.com>
2023-06-06 15:47:39 +0200

    He agregado los autores y he descomentado

--- src/JOIN.cpp
+++ src/JOIN.cpp
@@ -5,29 +5,27 @@
 //
 ///** 
 // * @file JOIN.cpp
-// * @author Silvia Acid Carrillo <acid@decsai.ugr.es>
-// * @author Andrés Cano Utrera <acu@decsai.ugr.es>
-// * @author Luis Castillo Vidal <L.Castillo@decsai.ugr.es>
-// * 
-// * Created on 29 January 2023, 11:00
-// */
+// * @author Francisco Javier Cáceres Martín <javicaceres@correo.ugr.es>
+// * @author Isaac Morales Santana <imorsan@correo.ugr.es>
+
 //// LO HACE ISAAC
 //
-///**
-// * Shows help about the use of this program in the given output stream
-// * @param outputStream The output stream where the help will be shown (for example,
-// * cout, cerr, etc) 
-// */
-//void showEnglishHelp(ostream& outputStream) {
-//    outputStream << "Error, run with the following parameters:" << endl;
-//    outputStream << "JOIN [-t|-b] [-o <outputFile.bgr>] <file1.bgr> [<file2.bgr> ... <filen.bgr>] " << endl;
-//    outputStream << "       join the Language files <file1.bgr> <file2.bgr> ... into <outputFile.bgr>" << endl;
-//    outputStream << endl;
-//    outputStream << "Parameters:" << endl;
-//    outputStream << "-t|-b: text mode or binary mode for the output file (-t by default)" << endl;
-//    outputStream << "-o <outputFile.bgr>: name of the output file (output.bgr by default)" << endl;
-//    outputStream << "<file*.bgr>: each one of the files to be joined" << endl;
-//}
+
+/**
+ * Shows help about the use of this program in the given output stream
+ * @param outputStream The output stream where the help will be shown (for example,
+ * cout, cerr, etc) 
+ */
+void showEnglishHelp(ostream& outputStream) {
+    outputStream << "Error, run with the following parameters:" << endl;
+    outputStream << "JOIN [-t|-b] [-o <outputFile.bgr>] <file1.bgr> [<file2.bgr> ... <filen.bgr>] " << endl;
+    outputStream << "       join the Language files <file1.bgr> <file2.bgr> ... into <outputFile.bgr>" << endl;
+    outputStream << endl;
+    outputStream << "Parameters:" << endl;
+    outputStream << "-t|-b: text mode or binary mode for the output file (-t by default)" << endl;
+    outputStream << "-o <outputFile.bgr>: name of the output file (output.bgr by default)" << endl;
+    outputStream << "<file*.bgr>: each one of the files to be joined" << endl;
+}
 //
 ///**
 // * This program reads an undefined number of Language objects from the files
