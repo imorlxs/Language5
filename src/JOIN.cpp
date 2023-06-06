8bbc5bdad0e717ae92ba1510da6475c34f2acdb0
imorlxs <isaacmoralessantana@gmail.com>
2023-06-06 15:48:57 +0200

    Descomentadas cosicas y agregada fstream

--- src/JOIN.cpp
+++ src/JOIN.cpp
@@ -10,8 +10,10 @@
 
 //// LO HACE ISAAC
 //
-#include<iostream>
+#include <iostream>
+#include <fstream>
 using namespace std;
+
 /**
  * Shows help about the use of this program in the given output stream
  * @param outputStream The output stream where the help will be shown (for example,
@@ -27,21 +29,21 @@ void showEnglishHelp(ostream& outputStream) {
     outputStream << "-o <outputFile.bgr>: name of the output file (output.bgr by default)" << endl;
     outputStream << "<file*.bgr>: each one of the files to be joined" << endl;
 }
-//
-///**
-// * This program reads an undefined number of Language objects from the files
-// * passed as parameters to main(). It obtains as result the union of all the 
-// * input Language objects. The result is then sorted by decreasing order of 
-// * frequency and alphabetical order of bigrams when there is any tie in 
-// * frequencies. Finally, the resulting Language is saved in an output file. The 
-// * program must have at least an output file. 
-// * Running example:
-// * >  JOIN [-t|-b] [-o <outputFile.bgr>] <file1.bgr> [<file2.bgr> ... <filen.bgr>]
-// * @param argc The number of command line parameters
-// * @param argv The vector of command line parameters (cstrings)
-// * @return 0 If there is no error; a value > 0 if error
-// */
-//int main(int argc, char* argv[]) {
-// 
-//}
-//
+
+/**
+ * This program reads an undefined number of Language objects from the files
+ * passed as parameters to main(). It obtains as result the union of all the 
+ * input Language objects. The result is then sorted by decreasing order of 
+ * frequency and alphabetical order of bigrams when there is any tie in 
+ * frequencies. Finally, the resulting Language is saved in an output file. The 
+ * program must have at least an output file. 
+ * Running example:
+ * >  JOIN [-t|-b] [-o <outputFile.bgr>] <file1.bgr> [<file2.bgr> ... <filen.bgr>]
+ * @param argc The number of command line parameters
+ * @param argv The vector of command line parameters (cstrings)
+ * @return 0 If there is no error; a value > 0 if error
+ */
+int main(int argc, char* argv[]) {
+ 
+}
+
